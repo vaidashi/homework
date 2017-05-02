9fe47882c2f4199931b7fedfaf9c0ee3755633c0
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-05-02 09:09:53 -0600

    initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,29 @@
+## Take shoe and hold left lace in left hand and right lace in right hand
+## Curl left lace in half and wrap right lace round curled left lace
+## Pull right lace through loop in left curled lace
+## While holding left lace, curl right lace in half
+## Pull curled right lace around left curled lace and loop through center
+
+#Common Words
+#1. Problem is solved when the 3 most common words in a given text are identified
+#2. Use software to create a method/fucntion
+#3. If the text only contains 3 words or less than that
+#4. Draft a function to create two arrays, for every word it places in a specific array
+#5. Then loops through text and if subsequent words are new it places them in a separate array
+#6. If word matches a given words in the first array it puts in that first array and we increment += a assigned variable by 1?
+#7. If a word matches one of the excluded words, it ignores them
+
+
+#Odds & Evens
+#1 When we have two files each correctly containing their respective numbers
+#2. Will create a function to search through list of numbers
+#3.
+#4. Create a function that searches the list of numbers and places numbers evenly
+####divisible by 2 in one list and all others in a separate list
+
+
+#Lats and Longs
+#1. When the smallest (lowest number pair) of coordinates are identified
+#2. To create a list that we can manipulate
+#3. If by chance the coordinates of O,O exists then immediately return that if true and exit method
+#4. Create an array and then sort in either decreasing or increasing order
